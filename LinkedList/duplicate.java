2a25a5599e59d30dc2c041e6424366da023811c4
Veeresh-Baddur <baddurveeresh@gmail.com>
2026-01-21 23:01:54 +0530

    Add program logic of remove duplicates in sorted linkedlist

---
+++ LinkedList/duplicate.java
@@ -0,0 +1,10 @@
+static void removeDuplicates(Node head) {
+    Node curr = head;
+
+    while (curr != null && curr.next != null) {
+        if (curr.data == curr.next.data)
+            curr.next = curr.next.next;
+        else
+            curr = curr.next;
+    }
+}
